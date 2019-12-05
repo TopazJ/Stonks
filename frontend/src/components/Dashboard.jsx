c100cd4f675de48b36948bc33502039903af11b9
TopazJ <shamez.meghji1@ucalgary.ca>
2019-12-05 01:36:26 -0700

    Put stuff in Dashboard.

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -1,10 +1,16 @@
-import {Component} from "react";
+import React, {Component} from "react";
 
 class Dashboard extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state={accounts:{}, stocks:{}}
+    }
+
+
     handleClick = (event) =>{
         console.log("yeet");
-        fetch('http://127.0.0.1:8000/auth/status/', {
+        fetch('http://127.0.0.1:8000/api/get-accounts/', {
             method: 'GET'
         }).then(res => res.json())
         .then(data => console.log(data))
@@ -12,9 +18,13 @@ class Dashboard extends Component {
 
     };
 
-
     render() {
-        <p>Dashboard</p>
+        return <div>
+            <p>
+            Dashboard
+            </p>
+            <button onClick={this.handleClick}>Get STONKS</button>
+        </div>;
     }
 }
 
