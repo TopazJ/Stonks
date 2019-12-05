588eda0ab81b1083dac6acfbf28b0c9200138395
Victor <victor.sanchezalarco@ucalgary.ca>
2019-12-04 23:41:47 -0700

    added some stuff to me algorithm

--- backend/Victor's Algorithm.py
+++ backend/Victor's Algorithm.py
@@ -2,4 +2,5 @@ from backend.logic import *
 
 def the_big_boi (username, account_no, start):
     user_account = get_user_account(username, account_no)
-    user_account.
+    transactions = get_transactions_by_account(user_account)
+    buy = transactions.filter()
